be16018f3c0b6d9dfb3d91a89534900e40ff8385
Tiaguin061 <tiaguin180@gmail.com>
2020-12-05 20:34:09 -0300

    Adding Header

--- src/pages/components/Header/Header.jsx
+++ src/pages/components/Header/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-import {Link} from 'react-router-dom'
-import './header.css'
+import {Link} from 'react-router-dom';
+import './header.css';
 
+import {FiMenu} from 'react-icons/fi';
+import {MdArrowForward} from 'react-icons/md';
+
+const menu = document.querySelector('#menu')
+const getButtonMenuOpen = document.querySelector('.btnMenu_open')
+const getButtonMenuClose = document.querySelector('.btnMenu_close')
+
+getButtonMenuOpen.addEventListener('click', function() {
+    menu.classList.add('menu_open')
+})  
+
+getButtonMenuClose.addEventListener('click', function() {
+    menu.classList.remove('menu_open')
+})  
 
 export default function Header() {
     return (
@@ -14,11 +28,43 @@ export default function Header() {
                         <h2>Gonçalves</h2>
                     </Link>
                 </div>
-                {/* <div className="navigation">
-                    <a href="#projects">Projetos</a>
-                    <a href="#tragetory">Sobre mim</a>
-                    <a href="#contact">Contato</a>
-                </div> */}
+                <li 
+                    className="btnMenu btnMenu_open">
+                    <FiMenu className="buttonMenu"/>
+                </li>
+                <ul id="menu" className="menu">
+                    <div className="navigation">
+                        <div className="logoBtnMenu">
+                            <div className="maxWd">
+                                <div className="logo">
+                                    <Link to="/">
+                                        <h1>Tiago</h1>
+                                        <h2>Gonçalves</h2>
+                                    </Link>
+                                </div>
+                                <li className="btnMenu btnMenu_close"><FiMenu className="buttonMenu"/></li>
+                            </div>
+                        </div>
+                        <li className="menu-item">
+                            <a href="#projects"
+                            className="menu-item-action">
+                                <MdArrowForward/> Projetos
+                            </a>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#tragetory" 
+                            className="menu-item-action">
+                               <MdArrowForward/> Sobre mim
+                            </a>
+                        </li>
+                        <li className="menu-item">
+                            <a href="#contact"
+                            className="menu-item-action">
+                               <MdArrowForward/> Contato
+                            </a>
+                        </li>
+                    </div>
+                </ul>
             </div>
         </header>
     )
