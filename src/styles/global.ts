6ea9f76eacf0c7eebccc50022d9b7939e0caad60
Tiaguin061 <tiaguin180@gmail.com>
2021-03-26 19:02:05 -0300

    feat: Creating global style file

---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@1,300;1,400&family=Rubik:wght@400;500;600;700;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;300;400;600&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color:  #d3e4ff;
+    list-style: none;
+    font-family: 'Rubik', sans-serif;
+  }
+
+  body {
+    background: rgb(36, 38, 56);
+    background: black;
+    width: 100%;
+    height: 100vh;
+    scroll-behavior: smooth;
+  }
+
+  a {
+    scroll-behavior: smooth;
+  }
+
+  :root {
+    --color-blue: #1d8eca;
+    --color-orange: #db8625;
+
+    --color-js:#f6da3a;
+    --color-node: #8cc84b;
+    --color-html: #e34f26;
+    --color-css: #2965f1;
+    --color-postgres: #336791;
+    --color-react:  #2ac7e3;
+    --color-rn: #5667f9;
+    font-size: 1rem;
+  }
+`;
