aee86821ff80713693cdbcfeb8be9f5b15feb8ca
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-05 23:06:40 -0300

    refactor: Removed data not used

--- src/Json/langs.tsx
+++ src/Json/langs.tsx
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
-import { DiJavascript1, DiNodejsSmall, DiHtml5, DiCss3, DiPostgresql, DiReact, DiDatabase } from 'react-icons/di';
-import { SiTypescript } from 'react-icons/si'; 
+import { DiJavascript1, DiNodejsSmall, DiPostgresql, DiReact, DiDatabase } from 'react-icons/di';
+import { SiTypescript, SiStyledComponents } from 'react-icons/si'; 
 
 interface IPropsItems {
   id: string;
@@ -55,47 +55,40 @@ export const domination: IPropsItems[] = [
   },
   {
     id: uuid(),
-    icon: <DiHtml5 />,
-    name: 'Html 5',
-    backgroundIcon: '#e34f26',
-    borderBottom: '1px solid #e34f26'
-  },
-  {
-    id: uuid(),
-    icon: <DiCss3 />,
-    name: 'Css 3',
-    backgroundIcon: '#2965f1',
-    borderBottom: '1px solid #2965f1'
+    icon: <SiStyledComponents />,
+    name: 'Styled Components',
+    backgroundIcon: '#d762e6',
+    borderBottom: '1px solid #d762e6'
   },
 ];
 
-export const learning: IPropsItems[] = [
-  {
-    id: uuid(),
-    icon: <DiReact />,
-    name: 'React Js',
-    backgroundIcon: '#2ac7e3',
-    borderBottom: '1px solid #2ac7e3'
-  },
-  {
-    id: uuid(),
-    icon: <DiNodejsSmall />,
-    name: 'Node Js',
-    backgroundIcon: '#8cc84b',
-    borderBottom: '1px solid #8cc84b'
-  },
-  {
-    id: uuid(),
-    icon: <SiTypescript />,
-    name: 'Typescript',
-    backgroundIcon: '#2281cf',
-    borderBottom: '1px solid #2281cf'
-  },
-  {
-    id: uuid(),
-    icon: <DiReact />,
-    name: 'React Native',
-    backgroundIcon: '#5667f9',
-    borderBottom: '1px solid #5667f9'
-  }
-];
+// export const learning: IPropsItems[] = [
+//   {
+//     id: uuid(),
+//     icon: <DiReact />,
+//     name: 'React Js',
+//     backgroundIcon: '#2ac7e3',
+//     borderBottom: '1px solid #2ac7e3'
+//   },
+//   {
+//     id: uuid(),
+//     icon: <DiNodejsSmall />,
+//     name: 'Node Js',
+//     backgroundIcon: '#8cc84b',
+//     borderBottom: '1px solid #8cc84b'
+//   },
+//   {
+//     id: uuid(),
+//     icon: <SiTypescript />,
+//     name: 'Typescript',
+//     backgroundIcon: '#2281cf',
+//     borderBottom: '1px solid #2281cf'
+//   },
+//   {
+//     id: uuid(),
+//     icon: <DiReact />,
+//     name: 'React Native',
+//     backgroundIcon: '#5667f9',
+//     borderBottom: '1px solid #5667f9'
+//   }
+// ];
